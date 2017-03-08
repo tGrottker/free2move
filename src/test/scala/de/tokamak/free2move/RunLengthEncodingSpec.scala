6246defce390b65908383801d83f4d4b95c55221
Thomas Grottker <thomas.grottker@webtrekk.com>
2017-03-08 11:01:30 +0100

    add test for run-length encoding with mixed elements

--- src/test/scala/de/tokamak/free2move/RunLengthEncodingSpec.scala
+++ src/test/scala/de/tokamak/free2move/RunLengthEncodingSpec.scala
@@ -5,14 +5,22 @@ import org.specs2._
 class RunLengthEncodingSpec extends Specification { def is = s2"""
   A RunLengthEncoder should
     reduce a collection of the same items to one entry            $encodeSame
+    encode a mixed number of consecutive elements accordingly     $encodeMixed
   """
 
   private val aStream: Stream[String] = "a" #:: aStream
+  private val bStream: Stream[String] = "b" #:: bStream
   private val sameElements = aStream.take(20)
+  private val mixedElements = sameElements ++ "b" #:: sameElements ++ bStream.take(10)
 
   def encodeSame = {
     val encoded = Seq(Repeat(20, "a"))
     RunLengthEncoding.compress(sameElements) must_== encoded
+  }
+
+  def encodeMixed = {
+    val encoded = Seq(Repeat(20, "a"), Single("b"), Repeat(20, "a"), Repeat(10, "b"))
+    RunLengthEncoding.compress(mixedElements) must_== encoded
   }
 
 }
